7f63b8b814d57c9bebedc9a490dd5a3452330879
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2023-11-22 18:33:01 +0200

    Test

--- lib/nitroux/utils.ex
+++ lib/nitroux/utils.ex
@@ -1,19 +1,17 @@
 defmodule Nitroux.Utils do
   @type tag :: String.t()
-  @spec tag(any, Nitroux.Types.GlobalAttributes.t() | [tag] | tag, any) :: tag()
+  @spec tag(any, Nitroux.Types.GlobalAttributes.t() | [tag] | tag, boolean()) :: tag()
   @doc """
-   Generates dynamic open and closing tags around content
-    iex> Nitroux.Utils.tag("div", ["hello", " ", "world"])
-    "<div>hello world</div>"
+  Generates dynamic open and closing tags around content
+  iex> Nitroux.Utils.tag("div", [])
+  "<div></div>"
 
-    Nitroux.Utils.tag("div", [])
-    "<div></div>"
+  iex> Nitroux.Utils.tag("div", ["hello world"])
+  "<div>hello world</div>"
 
-    Nitroux.Utils.tag("div", [html: "hello world"])
-    "<div>hello world</div>"
+  iex> Nitroux.Utils.tag("div", ["hello", " ", "world"])
+  "<div>hello world</div>"
 
-    Nitroux.Utils.tag("div", [class: "test", html: "hello world"])
-    "<div class=\"test\">hello world</div>"
   """
   def tag(name, attrs, container \\ true)
   def tag(name, attrs, false), do: "<#{name}#{add_attributes(attrs)}/>"
@@ -23,7 +21,6 @@ defmodule Nitroux.Utils do
     do: "<#{name}#{add_attributes(keywordlist)}>#{Keyword.get(keywordlist, :html, "")}</#{name}>"
 
   def tag(name, [_h | _t] = list, _container), do: name |> tag(html: Enum.join(list))
-
   def tag(name, text, _) when is_binary(text), do: "<#{name}>#{text}</#{name}>"
 
   defp add_attributes(attrs) do
