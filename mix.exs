cdcb441fc2900f895d681f98fcee93185ac9dba2
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2023-12-10 15:52:37 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nitroux.MixProject do
   def project do
     [
       app: :nitroux,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.13",
       name: "nitroux",
       description: description(),
